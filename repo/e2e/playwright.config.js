05a2f353c9e9af45c4086d5ff8df97bb93eb97e1
Aiman Ashber <aiman.ashber@chromaway.com>
2026-04-17 22:37:13 +0300

    playwrite

--- repo/e2e/playwright.config.js
+++ repo/e2e/playwright.config.js
@@ -5,6 +5,9 @@ module.exports = {
   use: {
     baseURL: process.env.BASE_URL || 'https://localhost:8443',
     ignoreHTTPSErrors: true,
-    headless: true
+    headless: true,
+    launchOptions: {
+      executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH || undefined
+    }
   }
 };
